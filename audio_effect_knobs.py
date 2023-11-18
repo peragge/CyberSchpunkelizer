a3473e2c2683081d4a9816efcd529108510630ce
Per Agge <44443024+peragge@users.noreply.github.com>
2023-11-17 19:01:21 -0800

    Create audio_effect_knobs.py

---
+++ audio_effect_knobs.py
@@ -0,0 +1,3 @@
+# Script for adjusting audio effects by turning knobs
+#To achieve dynamic adjustments to audio parameters using knobs, you can use the pydub library for audio manipulation and the RPi.GPIO library for handling GPIO inputs. Make sure to install pydub by running "pip install pydub
+"
