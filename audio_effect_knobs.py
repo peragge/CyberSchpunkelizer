bf4f293b06e28a2357da17d488f0b14872386713
Per Agge <44443024+peragge@users.noreply.github.com>
2023-11-17 19:02:38 -0800

    Update audio_effect_knobs.py

--- audio_effect_knobs.py
+++ audio_effect_knobs.py
@@ -1,3 +1,53 @@
 # Script for adjusting audio effects by turning knobs
-#To achieve dynamic adjustments to audio parameters using knobs, you can use the pydub library for audio manipulation and the RPi.GPIO library for handling GPIO inputs. Make sure to install pydub by running "pip install pydub
-"
+#To achieve dynamic adjustments to audio parameters using knobs, you can use the pydub library for audio manipulation and the RPi.GPIO library for handling GPIO inputs. Make sure to install pydub by running "pip install pydub"
+# Here's a basic example script:
+
+import RPi.GPIO as GPIO
+from pydub import AudioSegment
+from pydub.playback import play
+import time
+
+# Set up GPIO
+GPIO.setmode(GPIO.BOARD)
+knob1_pin = 17  # Replace with the GPIO pin for Knob1
+knob2_pin = 18  # Replace with the GPIO pin for Knob2
+knob3_pin = 19  # Replace with the GPIO pin for Knob3
+
+GPIO.setup(knob1_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(knob2_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(knob3_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Load your audio file
+audio_file = AudioSegment.from_file("your_audio_file.mp3")  # Replace with your audio file
+
+# Initial parameters
+pitch_factor = 1.0
+tempo_factor = 1.0
+reverb_factor = 0.0
+
+def play_audio():
+    global pitch_factor, tempo_factor, reverb_factor
+    adjusted_audio = (
+        audio_file
+        .speedup(playback_speed=tempo_factor)
+        .set_frame_rate(int(audio_file.frame_rate * pitch_factor))
+        .apply_gain(reverb_factor)
+    )
+    play(adjusted_audio)
+
+try:
+    while True:
+        if GPIO.input(knob1_pin) == GPIO.LOW:
+            pitch_factor += 0.1
+            play_audio()
+            time.sleep(0.2)  # Adjust debounce delay
+        elif GPIO.input(knob2_pin) == GPIO.LOW:
+            tempo_factor += 0.1
+            play_audio()
+            time.sleep(0.2)
+        elif GPIO.input(knob3_pin) == GPIO.LOW:
+            reverb_factor += 5.0
+            play_audio()
+            time.sleep(0.2)
+except KeyboardInterrupt:
+    GPIO.cleanup()
