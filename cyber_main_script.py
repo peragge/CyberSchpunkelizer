f992386f355b88c9b459b71525fccd58aab5e9c9
Per Agge <44443024+peragge@users.noreply.github.com>
2023-11-17 18:59:18 -0800

    Create cyber_main_script.py

---
+++ cyber_main_script.py
@@ -0,0 +1,2 @@
+# Import individual scripts on startup
+# To run at startup, open the crontab editor:   bash "crontab -e:  and add the following line to the crontab file:  "@reboot sleep 10 && python /path/to/your/script.py"
